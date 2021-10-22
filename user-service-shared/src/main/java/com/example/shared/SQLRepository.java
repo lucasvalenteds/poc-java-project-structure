3e9c4d43fa8fd9fa92e5c959ffcf42d7a1334c1b
Lucas <lucasvalenteds@gmail.com>
2021-10-22 20:45:12 -0300

    Overriding default SQLRepository construtor

--- user-service-shared/src/main/java/com/example/shared/SQLRepository.java
+++ user-service-shared/src/main/java/com/example/shared/SQLRepository.java
@@ -2,6 +2,9 @@ package com.example.shared;
 
 public abstract class SQLRepository {
 
+    protected SQLRepository() {
+    }
+
     protected static String toLikeFull(Object value) {
         return "%" + value + "%";
     }
