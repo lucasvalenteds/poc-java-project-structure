820dafe24d28fcc66b46a471bf2ce18bc6defec0
Lucas <lucasvalenteds@gmail.com>
2021-10-21 23:55:21 -0300

    Removing unnecessary consctrutor in SQLRepository

--- user-service-shared/src/main/java/com/example/shared/SQLRepository.java
+++ user-service-shared/src/main/java/com/example/shared/SQLRepository.java
@@ -2,9 +2,6 @@ package com.example.shared;
 
 public abstract class SQLRepository {
 
-    protected SQLRepository() {
-    }
-
     protected static String toLikeFull(Object value) {
         return "%" + value + "%";
     }
