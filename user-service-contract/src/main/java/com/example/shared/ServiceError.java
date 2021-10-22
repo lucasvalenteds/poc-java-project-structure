4ca3650e89dba8a38123a2036b02f93755b0930b
Lucas <lucasvalenteds@gmail.com>
2021-10-22 20:34:41 -0300

    ServiceError implementing Serializable

--- user-service-contract/src/main/java/com/example/shared/ServiceError.java
+++ user-service-contract/src/main/java/com/example/shared/ServiceError.java
@@ -1,4 +1,6 @@
 package com.example.shared;
 
-public final record ServiceError(Integer code, String message) {
+import java.io.Serializable;
+
+public final record ServiceError(Integer code, String message) implements Serializable {
 }
