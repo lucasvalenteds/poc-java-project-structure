270c7c258c9bddca9ee23e04d3aa84aa0952173e
Lucas <lucasvalenteds@gmail.com>
2021-10-22 00:41:35 -0300

    Adding jackson-core to client dependencies

--- user-service-client/build.gradle.kts
+++ user-service-client/build.gradle.kts
@@ -2,6 +2,7 @@ dependencies {
     implementation(project(":user-service-contract"))
     implementation(project(":user-service-shared"))
 
+    implementation("com.fasterxml.jackson.core", "jackson-core", properties["version.jackson"].toString())
     implementation("com.fasterxml.jackson.core", "jackson-databind", properties["version.jackson"].toString())
     implementation("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310", properties["version.jackson"].toString())
 }
